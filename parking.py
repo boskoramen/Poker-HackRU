05a1713d5975e53239f05e6ee2ff07a51279da38
rzhou10 <rzhou10@gmail.com>
2019-03-09 19:00:31 -0500

    don't need py files, added html

--- parking.py
+++
@@ -1,9 +0,0 @@
-import requests
-import connection
-import json
-
-def park(id):
-    username = input("Username: ")
-    password = input("Password: ")
-    response = requests.get("https://api.smartcar.com/v1.0/vehicles/" + id, auth=(username, password))
-    print(response.json())
